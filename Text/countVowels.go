eee284eab28189c610cae274a99939efa7cc1d69
Anggiajuang Patria <anggiaj@gmail.com>
2013-11-22 08:13:24 +0700

    Count vowels

---
+++ Text/countVowels.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Input some random strings:")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	vowels := map[rune]int{'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+
+	s := scanner.Text()
+	for _, c := range strings.ToLower(s) {
+		if _, ok := vowels[c]; ok {
+			vowels[c]++
+		}
+	}
+
+	t := 0
+	for r, c := range vowels {
+		fmt.Println(string(r), "=", c)
+		t += c
+	}
+	fmt.Println("Total =", t)
+}
