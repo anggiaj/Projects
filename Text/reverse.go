8c702993fd5d72d2d4cdeb799e3d37bb3fb3daff
Anggiajuang Patria <anggiaj@gmail.com>
2013-11-23 06:49:46 +0700

    text/reverse

--- Text/reverse.go
+++ Text/reverse.go
@@ -1,37 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-/*
-func reverse(s string) string {
-	b := []rune(s)
-	for i := 0; i < len(b)/2; i++ {
-		j := len(b) - i - 1
-		b[i], b[j] = b[j], b[i]
-	}
-	return string(b)
-}
-
 func main() {
-	var input string
-	_, err := fmt.Scanln(&input)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("%s", reverse(input))
+	fmt.Println("Input some random strings:")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	s := []rune(scanner.Text())
 
+	for i := 0; i < len(s)/2; i++ {
+		j := len(s) - i - 1
+		s[i], s[j] = s[j], s[i]
+	}
 
-}
-*/
-func main() {
-	var in string
-	_, err := fmt.Scanln(&in)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for i := len(in) - 1; i >= 0; i-- {
-		fmt.Print(string(in[i]))
-	}
+	fmt.Println(string(s))
 }
